0788edbbace012addbd5e58713e1ce9b5d5422fe
Ousmane <yabaye075@mail.com>
2020-01-11 17:59:42 +0000

    le storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,10 +50,17 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path(),
+            'url' => env('APP_URL').'/public',
             'visibility' => 'public',
         ],
+
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app/public'),
+        //     'url' => env('APP_URL').'/storage',
+        //     'visibility' => 'public',
+        // ],
 
         's3' => [
             'driver' => 's3',
